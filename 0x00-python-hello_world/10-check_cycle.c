f7aec72be210e7c05050ec74364c9cf95b1374b9
Mxn-ptr <maxence0@hotmail.fr>
2021-12-20 11:24:29 +0100

    Add task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if the list has a cycle or not
+ * @list: list to check
+ * Return: 1 if there is a cycle, else 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *normal, *fast;
+
+	normal = list->next;
+	fast = list->next->next;
+
+	while (normal && fast && fast->next)
+	{
+		if (normal == fast)
+			return (1);
+
+		normal = normal->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
