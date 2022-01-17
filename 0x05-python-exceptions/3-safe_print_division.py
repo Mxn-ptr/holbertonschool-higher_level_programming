f56dec8ad4954f7f22bca76a77c308e52c1885ae
Mxn-ptr <maxence0@hotmail.fr>
2022-01-17 12:08:51 +0100

    Fix pycodestyle

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+from decimal import DivisionByZero
+from re import T
+
+
 def safe_print_division(a, b):
     try:
         div = a / b
-    except:
+    except (TypeError, DivisionByZero):
         div = None
     finally:
         print("Inside result: {}".format(div))
