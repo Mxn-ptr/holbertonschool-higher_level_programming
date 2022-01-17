eb25c8d322d92134e54f8b39896ab50a6684a6e8
Mxn-ptr <maxence0@hotmail.fr>
2022-01-17 12:04:33 +0100

    Add all mandatory tasks

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+from typing import Type
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for elements in range(0, x):
+        try:
+            print("{:d}".format(my_list[elements]), end="")
+            count += 1
+        except (ValueError, TypeError): 
+            pass
+    print("")
+    return count
