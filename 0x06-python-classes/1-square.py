d204eb8f9aa06456786925387ea16cdcb8e7a0d7
Mxn-ptr <maxence0@hotmail.fr>
2022-01-18 19:12:14 +0100

    fix doc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+
+" Define a ckass Square Empty "
+
+
 class Square:
-    """ Define a classe Square
-    Private instance attribute : size """
+    """ 
+    Define a classe Square
+    
+    Attribute:
+        size (int): size of the square 
+    """
     def __init__(self, size):
         self.__size = size
