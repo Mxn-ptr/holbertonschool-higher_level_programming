5c7cffef2c2e3bc4278e7c72c3453d359452c267
Mxn-ptr <maxence0@hotmail.fr>
2022-01-24 18:15:51 +0100

    Change task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,7 +22,6 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """ Function for change the witdh of the rectangle """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -36,7 +35,6 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """ Function for change the height of the rectangle """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
