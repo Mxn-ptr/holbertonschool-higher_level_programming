11374db1a911706b5fc54b6f11c685e29eacab11
Mxn-ptr <maxence0@hotmail.fr>
2022-01-26 10:54:11 +0100

    Task 101

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import numpy
+
+""" Define a function for multiplicate matrix """
+
+def lazy_matrix_mul(m_a, m_b):
+    """ lazy_matrix_mul
+        Args: 
+            m_a (matrix): first matrix
+            m_b (matrix): seconde matrix
+    Return: mul matrix """
+    return numpy.matmul(m_a, m_b)
