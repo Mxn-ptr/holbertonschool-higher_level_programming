a570cd9df57c030873f0ab22915f89b42669b875
Mxn-ptr <maxence0@hotmail.fr>
2022-04-21 16:26:47 +0200

    Fix task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that send a JSON POST request to see if if valid or not
-curl -s -X POST "Content-Type: application/json" -d "@$2" "$1"
+curl -s -X POST "Content-Type: application/json" -d "$(cat "$2")" "$1"
