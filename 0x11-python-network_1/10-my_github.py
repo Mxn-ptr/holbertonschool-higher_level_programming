d8a3f87b0cc9203429eac5df0010f764a40cd0a5
Mxn-ptr <maxence0@hotmail.fr>
2022-04-22 12:14:24 +0200

    Add all mandatory tasks

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+if __name__ == "__main__":
+    basic = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get('https://api.github.com/user', auth=basic)
+    print(r.json().get("id"))
