e8455551ebf69f8c3c0ae719d337eb5283ffcb53
Mxn-ptr <maxence0@hotmail.fr>
2022-05-17 11:26:05 +0200

    Add task 0 to 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const axios = require('axios');
+let count = 0;
+
+axios.get('https://swapi-api.hbtn.io/api/films')
+  .then(function (response){
+    for (let i = 0; i < response.data.results.length; i++) {
+      if (response.data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        count++;
+      }
+    }
+    console.log(count);
+  });
