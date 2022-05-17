ca1b86e5d92fdb8bd254d09276d34db851450fdd
Mxn-ptr <maxence0@hotmail.fr>
2022-05-17 11:27:30 +0200

    Fix semistandard'

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 let count = 0;
 
 axios.get('https://swapi-api.hbtn.io/api/films')
-  .then(function (response){
+  .then(function (response) {
     for (let i = 0; i < response.data.results.length; i++) {
       if (response.data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
         count++;
